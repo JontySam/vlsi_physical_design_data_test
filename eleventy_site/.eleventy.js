3a55bca2ee81a24f71aec9ab58cfd1a091c5d0df
JontySam <jontysam15@gmail.com>
2026-02-22 21:56:09 -0600

    change

--- eleventy_site/.eleventy.js
+++ eleventy_site/.eleventy.js
@@ -1,18 +1,6 @@
-const { DateTime } = require("luxon");
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ "../vlsi_physical_design_web/assets": "assets" });
-  eleventyConfig.addPassthroughCopy({ "../vlsi_physical_design_web/main.css": "main.css" });
-  eleventyConfig.addPassthroughCopy({ "../vlsi_physical_design_web/main.js": "main.js" });
-  eleventyConfig.addPassthroughCopy({ "../vlsi_physical_design_web/carousel.js": "carousel.js" });
-  eleventyConfig.addPassthroughCopy({ "../vlsi_physical_design_web/robots.txt": "robots.txt" });
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPassthroughCopy({ "../vlsi_physical_design_web/BingSiteAuth.xml": "BingSiteAuth.xml" });
-  eleventyConfig.addPassthroughCopy({ "../vlsi_physical_design_web/search-data.json": "search-data.json" });
-
-  eleventyConfig.addFilter("date", (dateObj, format = "yyyy-LL-dd") => {
-    if (!dateObj) return "";
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(format);
-  });
 
   return {
     dir: {
@@ -23,6 +11,6 @@ module.exports = function (eleventyConfig) {
     },
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
-    templateFormats: ["njk", "md", "html"],
+    templateFormats: ["njk", "md"],
   };
 };
